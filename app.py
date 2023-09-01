457621572b3960f7e75db805997d40b42929d953
ulloa-g <ulloasaavedragabriel@gmail.com>
2023-09-01 12:21:42 -0300

    successful login

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=options)
 driver.get("https://prenotami.esteri.it/")
+driver.find_element(by=By.ID, value="login-email").send_keys("my_personal_email@gmail.com")
+driver.find_element(by=By.ID, value="login-password").send_keys("my_custom_pswd")
+driver.find_element(by=By.CLASS_NAME, value="button.primary.g-recaptcha").click()
