b470a64a0c8ca9cbfb10f5b8e0fe221c8dfc53db
ulloa-g <ulloasaavedragabriel@gmail.com>
2023-09-13 20:26:43 -0300

    login function working perfect

--- app.py
+++ app.py
@@ -6,11 +6,19 @@
 """
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 
-options = webdriver.ChromeOptions()
-options.add_experimental_option("detach", True)
-driver = webdriver.Chrome(options=options)
-driver.get("https://prenotami.esteri.it/")
-driver.find_element(by=By.ID, value="login-email").send_keys("my_personal_email@gmail.com")
-driver.find_element(by=By.ID, value="login-password").send_keys("my_custom_pswd")
-driver.find_element(by=By.CLASS_NAME, value="button.primary.g-recaptcha").click()
+EMAIL = "x@gmail.com"
+PSW = "y"
+driver = webdriver.Chrome()
+
+def login(user_email, usr_pwd):
+    driver.get("https://prenotami.esteri.it/")
+    driver.find_element(by=By.ID, value="login-email").send_keys(user_email) 
+    driver.find_element(by=By.ID, value="login-password").send_keys(usr_pwd)
+    driver.find_element(by=By.CLASS_NAME, value="button.primary.g-recaptcha").click()
+    return
+
+login(EMAIL, PSW)
+time.sleep(5)
+driver.quit()
